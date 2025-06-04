24d03ca9cd5ecd6280459055ec19624afc5787ba
Theodore-000 <Teddytez000@gmail.com>
2025-06-04 16:02:39 +0300

    Added new component for branding theme

---
+++ Ekub_frontend/src/components/admin/brandingTheme/BrandingTheme.tsx
@@ -0,0 +1,9 @@
+import { BrandingSettings } from "../../ui/branding-settings";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 py-4">
+      <BrandingSettings />
+    </div>
+  );
+}

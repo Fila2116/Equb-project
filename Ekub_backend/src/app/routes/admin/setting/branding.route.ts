3497900f2b866ba54e39de9131fd0de972505150
Theodore-000 <Teddytez000@gmail.com>
2025-06-05 12:31:00 +0300

    changed route configuration for branding route

--- Ekub_backend/src/app/routes/admin/setting/branding.route.ts
+++ Ekub_backend/src/app/routes/admin/setting/branding.route.ts
@@ -1,29 +1,34 @@
 // src/app/routes/admin/setting/branding.route.ts
 
-import express from 'express';
-import { getBrandingConfig, upsertBrandingConfig } from '../../../controllers/admin/setting/branding.controller';
+import express from "express";
 import {
+  getBrandingConfig,
+  upsertBrandingConfig,
+  uploadBrandingImages,
   resetBrandingConfig,
-  updateBrandingColors
+  updateBrandingColors,
 } from "../../../controllers/admin/setting/branding.controller";
+
 import * as StaffAuthMiddleware from "../../../middlewares/staff-auth.middleware";
 import { Permissions } from "@prisma/client";
 
-
 const router = express.Router();
 
-router.get('/', getBrandingConfig);
-router.post('/', upsertBrandingConfig); // or use `.put` if you prefer
+router.get("/", getBrandingConfig);
+
+router.post("/", uploadBrandingImages, upsertBrandingConfig);
+
 router.delete(
   "/",
   StaffAuthMiddleware.verifyStaff,
-  StaffAuthMiddleware.restrictStaff(Permissions.setting),
+  StaffAuthMiddleware.restrictStaff(Permissions.staff),
   resetBrandingConfig
 );
+
 router.patch(
   "/color",
   StaffAuthMiddleware.verifyStaff,
-  StaffAuthMiddleware.restrictStaff(Permissions.setting),
+  StaffAuthMiddleware.restrictStaff(Permissions.staff),
   updateBrandingColors
 );
 
